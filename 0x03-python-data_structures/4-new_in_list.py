73531ceca336b1492afcd1d5f8573edfda7e7eb7
MaoScorpion21 <mao7021@hotmail.com>
2022-05-10 19:12:30 -0500

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    magic_counter = len(my_list)
-    if idx < 0 or idx > magic_counter - 1:
-        return "None"
-    Index = my_list[idx]
-    return Index
+def new_in_list(my_list, idx, element):
+    new_list = my_list[:]
+    if 0 <= idx < len(new_list):
+        new_list[idx] = element
+        return (new_list)
+    return (my_list)
