f51d7ffebcdc6125af886e5013f1ccd3b2bb1306
MaoScorpion21 <mao7021@hotmail.com>
2022-06-07 23:22:46 -0500

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# File: 5-base_geometry.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""
+This module defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
